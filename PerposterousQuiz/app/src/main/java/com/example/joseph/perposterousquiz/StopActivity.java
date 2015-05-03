271a3647e144722713bd1223a1720e9615a6dc16
jbenda43251 <bendajs@hotmail.com>
2015-05-03 18:06:20 -0500

    Got Stop Question Working

--- PerposterousQuiz/app/src/main/java/com/example/joseph/perposterousquiz/StopActivity.java
+++ PerposterousQuiz/app/src/main/java/com/example/joseph/perposterousquiz/StopActivity.java
@@ -1,26 +1,54 @@
 package com.example.joseph.perposterousquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 public class StopActivity extends ActionBarActivity {
 
-    ImageView clock_iv;
+    GridView clock_lay;
     ImageView hammer_iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop);
-        clock_iv = (ImageView) findViewById(R.id.clock_iv);
-        hammer_iv = (ImageView) findViewById(R.id.hammer_iv);
-        clock_iv.setImageResource(R.drawable.hammerclock);
-        hammer_iv.setImageResource(R.drawable.hammer);
+        clock_lay = (GridView) findViewById(R.id.gridview);
+        clock_lay.setAdapter(new ImageAdapter(this));
+        clock_lay.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                // Send intent to SingleViewActivity
+                if(position != 0) {
+                    Intent i =
+                            new Intent(getApplicationContext(), GameOverActivity.class);
+                    // Pass image index
+                    i.putExtra("id", position);
+                    startActivity(i);
+                }
+                else
+                {
+                    Intent i =
+                            new Intent(getApplicationContext(), MainActivity.class);
+                    // Pass image index
+                    i.putExtra("id", position);
+                    startActivity(i);
+                }
+            }
+        });
     }
 
 
@@ -44,5 +72,45 @@ public class StopActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.hammerclock, R.drawable.gameclock,
+                R.drawable.bedclock, R.drawable.crunchclock
+        };
     }
 }
